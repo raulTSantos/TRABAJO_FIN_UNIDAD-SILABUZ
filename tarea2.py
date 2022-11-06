76de6a5959115df120deca1086d053ea4e7c4c53
MIDEZA-22 <mijail1993.mdzl@gmail.com>
2022-11-05 20:35:56 -0500

    Operacion_1 Completado

--- tarea2.py
+++ tarea2.py
@@ -30,6 +30,42 @@ def getPokemon(name: str) -> None:
      print(f"URL: {data['sprites']['front_default']}")
      print("-------------------------------------------------------------------------------------------")
 
+def validate_input(lista: list[str]) -> str:
+    value= input("Ingrese una opcion(Ejem: 1): ").strip()
+    options=[str(element) for element in range(1,len(lista)+1)]
+    print("")
+    while value not in options:
+        print("La respuesta ingresada no es valida ")
+        value= input(f"Ingrese valores del 1 al {len(lista)}: ").strip()
+        print("")
+    return value
+
+def show_elements(lista):
+    for index,x in enumerate(lista,start=1):
+        print(f"{index}. {x} ")
+    print(f"Total de {len(lista)} resultados\n")
+
+def show_pokemons(lista: list[str]) -> None:
+    if len(lista) ==0:
+        print(f"Cantida de pokemones encontrados: 0")
+    else:
+        for index,element in enumerate(lista, start=1):
+            print(f"POKEMON {index}")
+            getPokemon(element)
+
+def operation_1():
+    try:
+        generation_json = getResponse(URL_GENERATION)
+        names_list=[x["name"] for x in generation_json['results']]
+        show_elements(names_list)
+        print("Escriba una las opciones para ver pokemones disponibles y presione ENTER\n")
+        value=validate_input(names_list)
+        data_json = getResponse(f"{URL_GENERATION}/{value}")
+        pokemon_species_list=[x["name"] for x in data_json['pokemon_species']]
+        show_pokemons(pokemon_species_list)
+    except Exception :
+        print("Surgio un errror")
+
 def show_operations():
     print("\n------------ MENU DE OPCIONES ------------")
     print("\n(1) Listar pokemons por generacion")
@@ -45,7 +81,7 @@ def show_operations():
         opcion= input("Ingrese nuevamente la opcion (Ejem: 1) : ").strip()
 
     if opcion == "1":
-        pass
+        operation_1()
     elif opcion == "2":
         pass
     elif opcion == "3":
